e8c6fd085de7ed51b2187f02e41998bae3d7b18f
AnilWijesinghe <anilanjuna@gmail.com>
2021-11-03 16:14:16 +0000

    passed test and function impment for invalid argument; input position is already filled

--- ticTacToeValidation/validation.js
+++ ticTacToeValidation/validation.js
@@ -2,6 +2,12 @@ const validation = (inputPosition,gameBoard)=>{
     let positions = inputPosition.split(' '); // 1 2
     if(positions[0]<1 || positions[0]>=3 || positions[1]<1 || positions[1]>=3)
         throw new Error('Position should be in 3*3 grid');
+    for(let row=0;row<gameBoard.length;row++){
+        for(let col=0;col<gameBoard[row].length;col++){
+            if(row===parseInt(positions[0]) && col===parseInt(positions[1]))
+                if(gameBoard[row][col]!==null) throw new Error('input position is already filled');
+        }
+    }
 
 };
 
