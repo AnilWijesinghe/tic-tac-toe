eb546ffd0bf864d0104d17ca0c510e6b4631a1df
AnilWijesinghe <anilanjuna@gmail.com>
2021-11-03 16:51:03 +0000

    passed test and funtion implementaion; Find winner from row check

--- ticTacToeDecision/gameWinner.js
+++ ticTacToeDecision/gameWinner.js
@@ -1,4 +1,12 @@
-const gameWinner = ()=>{
+const gameWinner = (gameBoard)=>{
+
+    for(let row=0; row<3; row++){
+        if(gameBoard[row][0] == null){
+            console.log('continue');
+        }else if(gameBoard[row][0] === gameBoard[row][1] && gameBoard[row][1] === gameBoard[row][2]){
+            return gameBoard[row][0];
+        }
+    }
 
 };
 
