4f8591df288dd3d966a8a1028436266e10a3e97e
AnilWijesinghe <anilanjuna@gmail.com>
2021-11-03 15:46:28 +0000

    unit tet for invalid argument not in grid

--- ticTacToeValidation/validation.test.js
+++ ticTacToeValidation/validation.test.js
@@ -1,5 +1,11 @@
 const validation = require('./validation');
 
 test("",()=>{
+    // Arrange
+    let inputPosition = '1 4';
+
+    // Act and assert
+    expect(()=>validation()).toThrow('Position should be in 3*3 grid');
+
 
 });
