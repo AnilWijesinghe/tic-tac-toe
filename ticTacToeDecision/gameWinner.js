96a0aa47309050f06b1639993fb93292172ba492
AnilWijesinghe <anilanjuna@gmail.com>
2021-11-03 17:12:43 +0000

    passed test and funtion implementaion; Find winner from diagonal check

--- ticTacToeDecision/gameWinner.js
+++ ticTacToeDecision/gameWinner.js
@@ -1,21 +1,22 @@
 const gameWinner = (gameBoard)=>{
 
     for(let row=0; row<3; row++){
-        if(gameBoard[row][0] == null) continue;
+        if(gameBoard[row][0] == null) break;
         else if(gameBoard[row][0] === gameBoard[row][1] && gameBoard[row][1] === gameBoard[row][2]) return gameBoard[row][0];
     }
 
     for(let column=0; column<3; column++){
-        if(gameBoard[0][column] == null){
-            console.log('continue')
-        }else if(gameBoard[0][column] === gameBoard[1][column] && gameBoard[1][column] === gameBoard[2][column]){
-            return gameBoard[0][column];
-        }
+        if(gameBoard[0][column] == null) break;
+        else if(gameBoard[0][column] === gameBoard[1][column] && gameBoard[1][column] === gameBoard[2][column]) return gameBoard[0][column];
     }
 
+    if(gameBoard[0][0] != null && gameBoard[0][0] === gameBoard[1][1] && gameBoard[0][0] === gameBoard[2][2]){
+        return gameBoard[0][0];
+    }
 
-
-
+    if(gameBoard[0][2] != null && gameBoard[0][2] === gameBoard[1][1] && gameBoard[0][2] === gameBoard[2][0]){
+        return gameBoard[0][2];
+    }
 
 };
 
