d5ba1314fed7c96c0ef0e31f2689a9df0522afb7
AnilWijesinghe <anilanjuna@gmail.com>
2021-11-03 17:00:56 +0000

    passed test and funtion implementaion; Find winner from column check

--- ticTacToeDecision/gameWinner.js
+++ ticTacToeDecision/gameWinner.js
@@ -1,12 +1,21 @@
 const gameWinner = (gameBoard)=>{
 
     for(let row=0; row<3; row++){
-        if(gameBoard[row][0] == null){
-            console.log('continue');
-        }else if(gameBoard[row][0] === gameBoard[row][1] && gameBoard[row][1] === gameBoard[row][2]){
-            return gameBoard[row][0];
+        if(gameBoard[row][0] == null) continue;
+        else if(gameBoard[row][0] === gameBoard[row][1] && gameBoard[row][1] === gameBoard[row][2]) return gameBoard[row][0];
+    }
+
+    for(let column=0; column<3; column++){
+        if(gameBoard[0][column] == null){
+            console.log('continue')
+        }else if(gameBoard[0][column] === gameBoard[1][column] && gameBoard[1][column] === gameBoard[2][column]){
+            return gameBoard[0][column];
         }
     }
+
+
+
+
 
 };
 
