57a40b3fa608bad3165d5adf12da804cd9266d1b
AnilWijesinghe <anilanjuna@gmail.com>
2021-11-03 16:11:03 +0000

    fail test for Invalid argument; input position is already filled

--- ticTacToeValidation/validation.test.js
+++ ticTacToeValidation/validation.test.js
@@ -1,11 +1,31 @@
 const validation = require('./validation');
 
-test("",()=>{
+let positionFilledArr;
+
+beforeEach(()=>{
+    positionFilledArr = [
+        ["X", "O", null],
+        ["X", "O", "O"],
+        ["X", null, "O"]
+    ];
+})
+
+afterEach(()=>{
+    jest.clearAllMocks();
+});
+
+test("Invalid argument; input position not in grid",()=>{
     // Arrange
     let inputPosition = '1 4';
 
     // Act and assert
     expect(()=>validation(inputPosition)).toThrow('Position should be in 3*3 grid');
+});
 
+test("Invalid argument; input position is already filled",()=>{
+    // Arrange
+    let inputPosition = '1 4';
 
+    // Act and assert
+    expect(()=>validation(inputPosition,null)).toThrow('input position is already filled');
 });
