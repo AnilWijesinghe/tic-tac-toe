12ee418cb1b17045be980ac8ba0bcead39766967
AnilWijesinghe <anilanjuna@gmail.com>
2021-11-03 18:12:16 +0000

    failed test;Check game win input by player

--- ticTacToe/ticTacToe.test.js
+++ ticTacToe/ticTacToe.test.js
@@ -1,5 +1,22 @@
-const ticTacToe = require('./ticTacToe');
+const gamePlay = require('./ticTacToe');
+const gameWinner = require('../ticTacToeDecision/gameWinner')
+const findWinner = jest.spyOn(gameWinner, 'gameWinner');
 
-test("",()=>{
+let winningGrid;
 
+beforeEach(()=>{
+    winningGrid = [['X','X','X'],['O','X','O'],['X','O','O']];
+})
+
+test("Check game win when input symbol by player",()=>{
+    //Arrange
+    //Act
+    const inputPositionToWin = gamePlay('0 3');
+
+    //Assert
+    expect(findWinner)
+        .toHaveBeenCalledWith(winningGrid);
+    expect(findWinner)
+        .toHaveBeenCalledTimes(1);
+    expect(inputPositionToWin).toBe('X is winner');
 });
