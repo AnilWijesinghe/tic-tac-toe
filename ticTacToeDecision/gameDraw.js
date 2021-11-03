c499cd14117d4a0cc849bb48f77bb6827eee6fbd
AnilWijesinghe <anilanjuna@gmail.com>
2021-11-03 17:38:42 +0000

    passed test and funtion implementaion
    Game draw; All cells are filled

--- ticTacToeDecision/gameDraw.js
+++ ticTacToeDecision/gameDraw.js
@@ -1,5 +1,10 @@
-const gameDraw = ()=>{
-
+const gameDraw = (gameBoard)=>{
+    for(let row=0;row<gameBoard.length;row++){
+        for(let col=0;col<gameBoard[row].length;col++){
+            if(gameBoard[row][col]===null) return false;
+        }
+    }
+    return true;
 }
 
 module.exports = gameDraw;
