612c357cd9cc763f60bfdc43939fe8f400877208
AnilWijesinghe <anilanjuna@gmail.com>
2021-11-04 13:00:06 +0000

    passed test for winning scenario 1st input X

--- ticTacToe/ticTacToe.js
+++ ticTacToe/ticTacToe.js
@@ -1,6 +1,28 @@
-const gamePlay = ()=>{
+const gameWinner = require("../ticTacToeDecision/gameWinner");
+let gameBoard = [[null,null,null],[null,null,null],[null,null,null]];
+let currentPlayer = 'X';
+
+const gamePlay = (inputPosition)=>{
+
+    let posArray = inputPosition.split(' ');
+    gameBoard[parseInt(posArray[0])][parseInt(posArray[1])]=currentPlayer;
+    const winner = gameWinner.findWinner(gameBoard);
+    if(currentPlayer==='X'){
+        currentPlayer = 'O';
+    }else{
+        currentPlayer = 'X';
+    }
+    console.log(winner);
+    if(winner!==null){
+        return winner[0]+" is the winner";
+    }else{
+        return ""
+    }
+
+
 
 };
 
-module.exports = gamePlay;
+
+module.exports = {gamePlay};
 
--- ticTacToe/ticTacToe.test.js
+++ ticTacToe/ticTacToe.test.js
@@ -1,6 +1,6 @@
-const gamePlay = require('./ticTacToe');
-const gameWinner = require('../ticTacToeDecision/gameWinner')
-const findWinner = jest.spyOn(gameWinner, 'gameWinner');
+const ticTacToe = require('./ticTacToe');
+const gameWinner = require('../ticTacToeDecision/gameWinner');
+const findWinner = jest.spyOn(gameWinner, 'findWinner');
 
 let winningGrid;
 
@@ -8,15 +8,19 @@ beforeEach(()=>{
     winningGrid = [['X','X','X'],['O','X','O'],['X','O','O']];
 })
 
-test("Check game win when input symbol by player",()=>{
-    //Arrange
-    //Act
-    const inputPositionToWin = gamePlay('0 3');
+describe("Test for winning scenario", () => {
+    test("Check 1st input X",()=>{
+        //Arrange
+        //Act
+        ticTacToe.gamePlay('0 2');
 
-    //Assert
-    expect(findWinner)
-        .toHaveBeenCalledWith(winningGrid);
-    expect(findWinner)
-        .toHaveBeenCalledTimes(1);
-    expect(inputPositionToWin).toBe('X is winner');
+        //Assert
+        expect(findWinner)
+            .toHaveBeenCalledWith([[null,null,'X'],[null,null,null],[null,null,null]]);
+        expect(findWinner)
+            .toHaveBeenCalledTimes(1);
+    });
+
 });
+
+
