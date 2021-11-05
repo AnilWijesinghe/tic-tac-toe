088e76b3a4edc7b3b441464fe1a2a60e5b9508a5
AnilWijesinghe <anilanjuna@gmail.com>
2021-11-05 01:28:42 +0000

    fail test; game draw

--- ticTacToe/ticTacToe.test.js
+++ ticTacToe/ticTacToe.test.js
@@ -2,11 +2,6 @@ const ticTacToe = require('./ticTacToe');
 const gameWinner = require('../ticTacToeDecision/gameWinner');
 const findWinner = jest.spyOn(gameWinner, 'findWinner');
 
-let winningGrid;
-
-beforeEach(()=>{
-    winningGrid = [['X','X','X'],['O','X','O'],['X','O','O']];
-})
 
 describe("Test for validation scenario", () => {
     test("Validation when input position not in 3*3 grid",()=>{
@@ -20,7 +15,7 @@ describe("Test for validation scenario", () => {
     });
 });
 
-describe("Test for winning scenario", () => {
+describe("Test for winning scenario,Validation when input already filled position", () => {
     test("Check 1st input X",()=>{
         //Arrange
         //Act
@@ -82,6 +77,99 @@ describe("Test for winning scenario", () => {
         expect(findWinner)
             .toHaveBeenCalledTimes(5);
         expect(inputPositionToWin).toBe('X is the winner')
+    });
+});
+
+describe("Test for game draw", () => {
+    test("Check 1st input X",()=>{
+        //Arrange
+        //Act
+        ticTacToe.gamePlay('0 0');
+        //Assert
+        expect(findWinner)
+            .toHaveBeenCalledWith([['X',null,null],[null,null,null],[null,null,null]]);
+        expect(findWinner)
+            .toHaveBeenCalledTimes(1);
+    });
+    test("Check 1st input O",()=>{
+        //Arrange
+        //Act
+        ticTacToe.gamePlay('1 1');
+        //Assert
+        expect(findWinner)
+            .toHaveBeenCalledWith([['X',null,null],[null,'O',null],[null,null,null]]);
+        expect(findWinner)
+            .toHaveBeenCalledTimes(2);
+    });
+    test("Check 2nd input X",()=>{
+        //Arrange
+        //Act
+        ticTacToe.gamePlay('2 0');
+        //Assert
+        expect(findWinner)
+            .toHaveBeenCalledWith([['X',null,null],[null,'O',null],['X',null,null]]);
+        expect(findWinner)
+            .toHaveBeenCalledTimes(3);
+    });
+    test("Check 2nd input O",()=>{
+        //Arrange
+        //Act
+        ticTacToe.gamePlay('1 0');
+        //Assert
+        expect(findWinner)
+            .toHaveBeenCalledWith([['X',null,null],['O','O',null],['X',null,null]]);
+        expect(findWinner)
+            .toHaveBeenCalledTimes(4);
+    });
+    test("Check 3rd input X",()=>{
+        //Arrange
+        //Act
+        const inputPositionToWin = ticTacToe.gamePlay('1 2');
+        //Assert
+        expect(findWinner)
+            .toHaveBeenCalledWith([['X',null,null],['O','O','X'],['X',null,null]]);
+        expect(findWinner)
+            .toHaveBeenCalledTimes(5);
+    });
+    test("Check 3rd input O",()=>{
+        //Arrange
+        //Act
+        const inputPositionToWin = ticTacToe.gamePlay('0 1');
+        //Assert
+        expect(findWinner)
+            .toHaveBeenCalledWith([['X','O',null],['O','O','X'],['X',null,null]]);
+        expect(findWinner)
+            .toHaveBeenCalledTimes(6);
+    });
+    test("Check 4th input X",()=>{
+        //Arrange
+        //Act
+        const inputPositionToWin = ticTacToe.gamePlay('2 1');
+        //Assert
+        expect(findWinner)
+            .toHaveBeenCalledWith([['X','O',null],['O','O','X'],['X','X',null]]);
+        expect(findWinner)
+            .toHaveBeenCalledTimes(7);
+    });
+    test("Check 4th input O",()=>{
+        //Arrange
+        //Act
+        const inputPositionToWin = ticTacToe.gamePlay('2 2');
+        //Assert
+        expect(findWinner)
+            .toHaveBeenCalledWith([['X','O',null],['O','O','X'],['X','X','O']]);
+        expect(findWinner)
+            .toHaveBeenCalledTimes(8);
+    });
+    test("Check 5th input X",()=>{
+        //Arrange
+        //Act
+        const inputPositionToWin = ticTacToe.gamePlay('0 2');
+        //Assert
+        expect(findWinner)
+            .toHaveBeenCalledWith([['X','O','X'],['O','O','X'],['X','X','O']]);
+        expect(findWinner)
+            .toHaveBeenCalledTimes(9);
     });
 });
 
